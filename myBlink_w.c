fbb71c4b2c8c98fb4345310d1908a13617e8190c
diegomutis98 <diego.mutis@udea.edu.co>
2024-03-29 23:39:51 -0500

    en el archivo myBlink_w.c ya se actualizo el proceso de medición del tiempo correctamente y final conclución del proyecto

--- myBlink_w.c
+++ myBlink_w.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+// libreias necesarias
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
@@ -18,8 +19,10 @@
 // Definimos los pines para los botones
 #define BUTTON_1 16 
 #define BUTTON_2 17
+#define BUTTON_3 18
+#define BUTTON_4 19
 
-// Pines reservados para los segmentos de los displays
+// Definimos los pines para los segmentos de los displays de 7-segmentos
 #define SEG_A 0
 #define SEG_B 1
 #define SEG_C 2
@@ -29,10 +32,10 @@
 #define SEG_G 6
 #define SEG_DP 7
 
-// Definimos los pines para los transistores
-#define TRAN_1 13
-#define TRAN_2 14
-#define TRAN_3 15
+// Definimos los pines para los displays (para la multiplexacion)
+#define D_1 13
+#define D_2 14
+#define D_3 15
 
 // Secuencia de estados de los LEDs
 #define SEQUENCE_LENGTH 4 // Longitud de la secuencia
@@ -43,186 +46,189 @@ bool led_states[SEQUENCE_LENGTH][3] = {
     {false, false, false} // "000"
 };
 
-// Índice actual de la secuencia
-int sequence_index = 5;
-int contador = 0;
-int contadorus = 0;
-int time_pessed = 0;
+// variables goblales para configurar los tiempos de conteo de los timers
+int sequence_index = 5; // indice de la secuencia inicial (arranca en un numero mayor que 4 para garantizar que que la secuencia se ralice correctamente)
+int contador = 0; // contador de segundos
+int contador_ms = 0; // contador en milisegundos
+int time_pessed = 0; // para medir el tiempo en que se demora el jugador en pesionar el boton
 
-// Definiciones para los temporizadores
-// Tiempo de actualización del display en milisegundos
-#define INTERVAL_US 1000000 // Intervalo de tiempo en microsegundos (1 segundo)
-
-#define INTERVAL_USd 5000 // Por ejemplo, actualiza cada 3 milisegundos
-volatile bool Band_dis = false;
-volatile uint32_t timer_ticks = 0; // Contador de ticks del temporizador
-uint8_t numD = 0;
-//volatile uint32_t timer_disp = 0; // Contador de ticks del temporizador
-
-
-// Función de interrupción del temporizador
-void timer_callback1(struct repeating_timer *t) {
-    timer_ticks++; // Incrementar el contador de ticks
-}
-void timer_callback2(struct repeating_timer *t) {
-    Band_dis = true;
-}
-
-bool to_do_sequence();
-uint8_t to_do_random(uint8_t rangoMin, uint8_t rangoMax);
-void encender_LED(uint8_t pindLed);
-
-/*
-// Función de interrupción del botón
-void button_callback() {
-    // Reiniciar el temporizador cuando se presiona el botón
-    struct repeating_timer timer3;
-    add_repeating_timer_ms(1000, timer_callback3, NULL, &timer3);
-    printf("Botón presionado.\n");
-}
-*/
-void timer_callback3(struct repeating_timer *t);
-void timer_callback4(struct repeating_timer *t);
-void timer_callback5(struct repeating_timer *t);
-void sieteSegmentos(const uint8_t numero);
-// Función para descomponer un entero de tres dígitos en unidades, decenas y centenas
-void descomponer_entero(int numero, u_int8_t *centenas, uint8_t *decenas, u_int8_t *unidades);
-
+// Declaration of funtions used in the program
+bool to_do_sequence(); // Function to perform the initial sequence
+uint8_t random_number(uint8_t rangoMin, uint8_t rangoMax); // Function that returns a random number in the set rangoMin and rangoMax
+void turn_LED(uint8_t pindLed); // function that turns a spesific LED on or off
+void timer_callback_1(struct repeating_timer *t); // function to count seconds
+void timer_callback_2(struct repeating_timer *t); // function to count seconds
+void timer_callback_3(struct repeating_timer *t); // function to count miliseconds
+void sieteSegmentos(const uint8_t numero); // function that show a segmentation
+void descompose_number(int numero, u_int8_t *centenas, uint8_t *decenas, u_int8_t *unidades); // functio to descompose a number in units, decimas and centecimas
 
 int main() {
 	stdio_init_all();
-    gpio_init(SEG_A); // Inicializar el pin del LED
-    gpio_init(SEG_B); // Inicializar el pin del LED
-    gpio_init(SEG_C); // Inicializar el pin del LED
-    gpio_init(SEG_D); // Inicializar el pin del LED
-    gpio_init(SEG_E); // Inicializar el pin del LED
-    gpio_init(SEG_F); // Inicializar el pin del LED
-    gpio_init(SEG_G); // Inicializar el pin del LED
-    gpio_init(SEG_DP); // Inicializar el pin del LED
-    gpio_set_dir(SEG_A, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_set_dir(SEG_B, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_set_dir(SEG_C, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_set_dir(SEG_D, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_set_dir(SEG_E, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_set_dir(SEG_F, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_set_dir(SEG_G, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_set_dir(SEG_DP, GPIO_OUT); // Establecer el pin del LED como salida
 
-    gpio_init(TRAN_1);
-    gpio_init(TRAN_2);
-    gpio_init(TRAN_3);
-    gpio_set_dir(TRAN_1, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_set_dir(TRAN_2, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_set_dir(TRAN_3, GPIO_OUT); // Establecer el pin del LED como salida
+    // configuramos los pines de los segmentos
+    gpio_init(SEG_A); 
+    gpio_init(SEG_B); 
+    gpio_init(SEG_C); 
+    gpio_init(SEG_D); 
+    gpio_init(SEG_E); 
+    gpio_init(SEG_F); 
+    gpio_init(SEG_G); 
+    gpio_init(SEG_DP); 
+    gpio_set_dir(SEG_A, GPIO_OUT); 
+    gpio_set_dir(SEG_B, GPIO_OUT); 
+    gpio_set_dir(SEG_C, GPIO_OUT); 
+    gpio_set_dir(SEG_D, GPIO_OUT); 
+    gpio_set_dir(SEG_E, GPIO_OUT); 
+    gpio_set_dir(SEG_F, GPIO_OUT); 
+    gpio_set_dir(SEG_G, GPIO_OUT); 
+    gpio_set_dir(SEG_DP, GPIO_OUT); 
 
+    // configuramos los pines de los displays
+    gpio_init(D_1);
+    gpio_init(D_2);
+    gpio_init(D_3);
+    gpio_set_dir(D_1, GPIO_OUT);
+    gpio_set_dir(D_2, GPIO_OUT);
+    gpio_set_dir(D_3, GPIO_OUT);
+
+    // configuramos los pines para los LEDs
+    gpio_init(LED_1);
+    gpio_init(LED_2);
+    gpio_init(LED_3);
+    gpio_set_dir(LED_1, GPIO_OUT);
+    gpio_set_dir(LED_2, GPIO_OUT);
+    gpio_set_dir(LED_3, GPIO_OUT);
     
-    // Inicializamos tres pines como salida para los LEDs
-    gpio_init(LED_1); // Inicializar el pin del LED
-    gpio_set_dir(LED_1, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_init(LED_2); // Inicializar el pin del LED
-    gpio_set_dir(LED_2, GPIO_OUT); // Establecer el pin del LED como salida
-    gpio_init(LED_3); // Inicializar el pin del LED
-    gpio_set_dir(LED_3, GPIO_OUT); // Establecer el pin del LED como salida
-
-    // Configurar el temporizador para generar una interrupción cada segundo
+    // configuramos los pines para los botones
+    gpio_init(BUTTON_1);
+    gpio_init(BUTTON_2);
+    gpio_init(BUTTON_3);
+    gpio_set_dir(BUTTON_1, GPIO_IN);
+    gpio_set_dir(BUTTON_2, GPIO_IN);
+    gpio_set_dir(BUTTON_3, GPIO_IN);
+    gpio_pull_down(BUTTON_1);
+    gpio_pull_down(BUTTON_2);
+    gpio_pull_down(BUTTON_3); 
     
+    // variables para las unidades, decimas, centecimas y milecimas
+    uint8_t u = 0;
+    uint8_t d = 0;
+    uint8_t c = 0;
+    uint8_t m = 0;
 
-    // configuracion del boton
-    gpio_init(BUTTON_1); // Inicializar el pin del LED
-    gpio_set_dir(BUTTON_1, GPIO_IN); // Establecer el pin del LED como salida
-    gpio_pull_down(BUTTON_1); // Inicializ
-    // configuracion del boton
-    gpio_init(BUTTON_2); // Inicializar el pin del LED
-    gpio_set_dir(BUTTON_2, GPIO_IN); // Establecer el pin del LED como salida
-    gpio_pull_down(BUTTON_2); // Inicializ
-    
-
-    uint8_t u = 9;
-    uint8_t d = 3;
-    uint8_t c = 4;
-
+    // variables para almacenar los numeros aleatorios de tiempo y led 
     uint8_t randomTime = 0;
     uint8_t randomLed = 0;
 
-    //clock_t lastUpdateTime = clock(); // Tiempo de la última actualización
-    // Configurar un temporizador para controlar la secuencia de LEDs
-    struct repeating_timer timer1, timer2, timer3, timer4, timer5;
-    add_repeating_timer_us(INTERVAL_US, timer_callback1, NULL, &timer1);
-    add_repeating_timer_us(INTERVAL_USd, timer_callback2, NULL, &timer2);
-    add_repeating_timer_ms(2000, timer_callback3, NULL, &timer3);
-    add_repeating_timer_ms(1000, timer_callback4, NULL, &timer4);
-    add_repeating_timer_ms(1000, timer_callback5, NULL, &timer5);
-    // Bucle infinito para mostrar números en los displays
-    uint16_t number = 1234;
+    // timers para controlar los tiempos de funcionamiento del programa
+    struct repeating_timer timer_1, timer_2, timer_3;
+    add_repeating_timer_ms(1000, timer_callback_1, NULL, &timer_1);
+    add_repeating_timer_ms(1000, timer_callback_2, NULL, &timer_2);
+    add_repeating_timer_ms(10, timer_callback_3, NULL, &timer_3);
 
-    bool flag_1 = false;
-    bool flag_2 = false;
-    bool flag_3 = false;
-    bool flag_4 = false;
+    // flags para conrolar las etapas de flujo del progrma
+    bool flag_1 = false; // hacer secuencia inicial
+    bool flag_2 = false; // parar secuencia inicial, generar numero aleatorio y esperar ese tiempo
+    bool flag_3 = false; // encender un led aleatoriamente y empesar a contar el tiempo depues de encender el led
+    bool flag_4 = false; // mostar el tiempo hasta que se precionó el boton correcto.
 
-    while (1)
-    {
+    // bucle prinsipal
+    while (1){
 
+        // hacer secuencia inicial
         if (gpio_get(BUTTON_1) || flag_1) {
             flag_1 = true;
             flag_2 = to_do_sequence();
-            printf("Botón presionado\n");
         }
+
+        // parar secuencia inicial, generar numero aleatorio y esparar ese tiempo
         if (flag_2){
             flag_1 = false;
             sequence_index = 5;
+            // Generamos un tiempo y pin para el led aleatoriamente
+            randomTime = random_number(0, 10);
+            randomLed = random_number(10, 12);
 
-            randomTime = to_do_random(0, 10);
-            randomLed = to_do_random(10, 12);
-
+            // Para esperar un tiempo generado aleatoriamente [Segundos]
             if (contador >= randomTime){
-                encender_LED(randomLed);
-                contadorus = 0;
+                turn_LED(randomLed);
+                contador_ms = 0;
                 flag_2 = false;
                 flag_3 = true;
             }
         }
+
+        //encender un led aleatoriamente y empesar a contar el tiempo depues de encender el led
         if(flag_3){
-            
-            descomponer_entero(contadorus, &c, &d, &u);
+            descompose_number(contador_ms, &c, &d, &u);
+
             sieteSegmentos(c);
-            gpio_put(TRAN_1, 1);
+            gpio_put(SEG_DP, 1);
+            gpio_put(D_1, 1);
             sleep_ms(5);
-            gpio_put(TRAN_1, 0);
+            gpio_put(D_1, 0);
+            gpio_put(SEG_DP, 0);
 
             sieteSegmentos(d);
-            gpio_put(TRAN_2, 1);
+            gpio_put(D_2, 1);
             sleep_ms(5);
-            gpio_put(TRAN_2, 0);
+            gpio_put(D_2, 0);
 
             sieteSegmentos(u);
-            gpio_put(TRAN_3, 1);
+            gpio_put(D_3, 1);
             sleep_ms(5);
-            gpio_put(TRAN_3, 0);
+            gpio_put(D_3, 0);
 
-            if(gpio_get(BUTTON_2)){
+            // Si presionamos el boton asociado al led 1
+            if(gpio_get(BUTTON_2) && gpio_get(LED_3)){
                 flag_3 = false;
                 flag_4 = true;
-                time_pessed = contadorus;
+                time_pessed = contador_ms;
                 sleep_us(5);
             }
+
+            // si presionamos el boton asociado al led 2
+            if(gpio_get(BUTTON_3) && gpio_get(LED_2)){
+                flag_3 = false;
+                flag_4 = true;
+                time_pessed = contador_ms;
+                sleep_us(5);
+            }
+
+            // si presionamos el boton asociado al led 3
+            if(gpio_get(BUTTON_3) && gpio_get(LED_1)){
+                flag_3 = false;
+                flag_4 = true;
+                time_pessed = contador_ms;
+                sleep_us(5);
+            }
+            /*
+            // si no se presiona el boton que es sumamos 1 segundo
+            if ((gpio_get(BUTTON_2) && !gpio_get(LED_3)) || (gpio_get(BUTTON_3) && !gpio_get(LED_2)) || (gpio_get(BUTTON_3) && !gpio_get(LED_1))){
+                sleep_ms(10);
+                contadorus += 100; 
+            }
+            */
         }
+        // mostar el tiempo hasta que se precionó el boton correcto.
         if(flag_4){
-            descomponer_entero(time_pessed, &c, &d, &u);
+            descompose_number(time_pessed, &c, &d, &u);
             sieteSegmentos(c);
-            gpio_put(TRAN_1, 1);
+            gpio_put(SEG_DP, 1);
+            gpio_put(D_1, 1);
             sleep_ms(5);
-            gpio_put(TRAN_1, 0);
+            gpio_put(D_1, 0);
+            gpio_put(SEG_DP, 0);
 
             sieteSegmentos(d);
-            gpio_put(TRAN_2, 1);
+            gpio_put(D_2, 1);
             sleep_ms(5);
-            gpio_put(TRAN_2, 0);
+            gpio_put(D_2, 0);
 
             sieteSegmentos(u);
-            gpio_put(TRAN_3, 1);
+            gpio_put(D_3, 1);
             sleep_ms(5);
-            gpio_put(TRAN_3, 0);
+            gpio_put(D_3, 0);
 
             gpio_put(randomLed, 0);
 
@@ -236,25 +242,23 @@ int main() {
     return 0;   
 }
 
-
-
-void timer_callback3(struct repeating_timer *t){
+void timer_callback_1(struct repeating_timer *t){
     // Incrementar el índice de la secuencia
     sequence_index++;
     // Reiniciar el índice si llegamos al final de la secuencia
 }
 
-void timer_callback4(struct repeating_timer *t){
+void timer_callback_2(struct repeating_timer *t){
     contador++;
     if (contador >= 999){
         contador = 0;
     }
 }
 
-void timer_callback5(struct repeating_timer *t){
-    contadorus++;
-    if (contadorus >= 999){
-        contadorus = 0;
+void timer_callback_3(struct repeating_timer *t){
+    contador_ms++;
+    if (contador_ms >= 1000){
+        contador_ms = 999;
     }
 }
 
@@ -276,7 +280,7 @@ bool to_do_sequence(){
     }
 }
 
-uint8_t to_do_random(uint8_t rangoMin, uint8_t rangoMax){
+uint8_t random_number(uint8_t rangoMin, uint8_t rangoMax){
     // Semilla para la función rand() basada en el tiempo actual
     srand(time(NULL));
 
@@ -289,7 +293,7 @@ uint8_t to_do_random(uint8_t rangoMin, uint8_t rangoMax){
     return numero_aleatorio;
 }
 
-void encender_LED(uint8_t pindLed){
+void turn_LED(uint8_t pindLed){
     switch (pindLed)
             {
             case 10:
@@ -315,8 +319,7 @@ void encender_LED(uint8_t pindLed){
             }
 }
 
-// Función para descomponer un entero de tres dígitos en unidades, decenas y centenas
-void descomponer_entero(int numero, u_int8_t *centenas, u_int8_t *decenas, u_int8_t *unidades) {
+void descompose_number(int numero, u_int8_t *centenas, u_int8_t *decenas, u_int8_t *unidades) {
     // Asegurarse de que el número sea de tres dígitos
     if (numero >= 0 && numero <= 999) {
         *centenas = numero / 100;
